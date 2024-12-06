f91783649f1b03955aefb9521c4ee4bd60f9510e
ethansmadjaa <ethansmadja@icloud.com>
2024-12-06 21:36:21 +0100

    ✨ Add default 'days' parameter to preprocess_data function

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,8 @@
 import streamlit as st
 from pyspark.sql.functions import *
 
-def preprocess_data(spark, ticker):
+
+def preprocess_data(spark, ticker, days: int | str = 365):
     st.subheader(f" Preprocessing Data for {ticker}")
     st.info("Preprocessing module is under development. Coming soon!")
     # Preprocessing logic will be implemented here
